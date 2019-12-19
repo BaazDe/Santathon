d09837c5851ababacb9d3939b1a7f37799252ba1
Gautier Mille <gautier.mille@gmail.com>
2019-12-19 09:35:22 +0100

    add router

--- frontend/santathon/src/app/app.module.ts
+++ frontend/santathon/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { FormulaireComponent } from "./formulaire/formulaire.component";
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "addagift", component: FormulaireComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { FormsModule } from "@angular/forms";
     FooterComponent,
     FormulaireComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
