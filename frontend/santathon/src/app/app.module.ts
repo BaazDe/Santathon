af992878eeab38476bc7362b604a6909b12f5733
Gautier Mille <gautier.mille@gmail.com>
2019-12-19 09:20:48 +0100

    resolve conflicts

--- frontend/santathon/src/app/app.module.ts
+++ frontend/santathon/src/app/app.module.ts
@@ -1,28 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-
-
-@NgModule({
-  declarations: [AppComponent, HeaderComponent, FormulaireComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-          import { FormulaireComponent } from "./formulaire/formulaire.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { FormulaireComponent } from "./formulaire/formulaire.component";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormulaireComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
